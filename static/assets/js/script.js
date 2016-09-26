66a64f1b3fd7ee9ae1615a2f2639219014015745
mukkachaitanya <chaitanya.mvs2007@gmail.com>
2016-09-26 13:30:10 +0530

    FrontEnd and Mobile Site

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,8 +1,8 @@
 //Courtesy https://github.com/danfickle/
 
-var myresize = function() {
-    var Width = 1280;
-    var Height = 742;
+var myresize = function(idName, reqWidth, reqHeight) {
+    var Width = reqWidth;
+    var Height = reqHeight;
     var ratioW = window.innerWidth / Width;
     var ratioH = window.innerHeight / Height;
 
@@ -10,24 +10,31 @@ var myresize = function() {
     var marginL = ((window.innerWidth - (ratio * Width)) / 2) * ratio;
     var marginT = ((window.innerHeight - (ratio * Height)) / 2) * ratio;
 
-    var divs = document.querySelectorAll('.my-center');
+    var divs = document.querySelectorAll(idName);
 
     Array.prototype.forEach.call(divs, function(el, i) {
+        console.log(el.id, i);
         el.style.transformOrigin = '0 0';
         el.style.transform = 'scale(' + ratio + ',' + ratio + ')';
-
-        el.style.left = marginL + 'px';
-        el.style.top = marginT + 'px';
 
         el.style.webkitTransformOrigin = '0 0';
         el.style.webkitTransform = 'scale(' + ratio + ',' + ratio + ')';
 
+        el.style.left = marginL + 'px';
+        if (idName != "#scaleDiv") {
+            el.style.top = marginT + 'px';
+        }
     });
 }
 
-window.onresize = myresize;
-myresize();
+function individualResize() {
+    myresize("#background", 1440, 720);
+    myresize("#scaleDiv", 1280, 600);
+}
+
+window.onresize = individualResize;
+individualResize();
 
 document.addEventListener('DOMContentLoaded', function() {
-    myresize();
+    individualResize();
 });
